d2e26932b396ba9976162e6a3593b1ee7315dbb5
dshupegin <DmTan2329>
2021-08-25 14:30:24 +0500

    25.08

---
+++ app/src/main/java/com/sushi/Sushi/service/Servise_notification.kt
@@ -0,0 +1,11 @@
+package com.sushi.Sushi.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class Servise_notification : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+    }
+}
